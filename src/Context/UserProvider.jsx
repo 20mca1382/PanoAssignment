8879427758b2a57799679f67a5e85e432572c2f4
20mca1382 <73593913+20mca1382@users.noreply.github.com>
2023-03-25 15:32:53 +0530

    completed

---
+++ src/Context/UserProvider.jsx
@@ -0,0 +1,34 @@
+// Import necessary dependencies
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react'
+
+export const userContext = createContext(); 
+
+const dataArr = JSON.parse(localStorage.getItem("userData"))
+
+const UserProvider = ({ children }) => {
+ 
+const [isloading, setIsLoading] = useState(false)
+const [users, setUsers] = useState([]);
+const getData = async () => {
+try {
+setIsLoading(true)
+const res = await axios.get('https://panorbit.in/api/users.json');
+const userData = res.data.users;
+setIsLoading(false)
+setUsers(userData);
+} catch (error) {
+console.error(error);
+setIsLoading(false)
+}
+};
+useEffect(() => {
+getData();
+}, []);
+return (
+<userContext.Provider value={{ users, getData, dataArr, isloading }}>
+{children}
+</userContext.Provider>
+)
+}
+export default UserProvider;
